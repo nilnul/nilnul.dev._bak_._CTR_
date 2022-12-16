c8639fc53d4b1e520ac569b61d1906e94c903712
WANG,Youtian <wangyoutian@nilnul.com>
2022-12-16 16:00:42 +0800

    update some deps; etc;

--- _cfg_/Push4nonchange.cs
+++ _cfg_/Push4nonchange.cs
@@ -19,16 +19,16 @@ namespace nilnul.dev._bak_._CTR_._cfg_
 
 		private void Push4nonchange_Load(object sender, EventArgs e)
 		{
-			this.checkBox1.Checked = nilnul.dev.bak.nilnul0.dev.src.sln.div_.unjoint_.nonneglect_.giT_.work_.top_._incomplete_.container_.undone_.wrought_.primed._bak_._upload.Settings1.Default.push4nonchange;
+			this.checkBox1.Checked = nilnul.dev.srcs.each.sln.div_.delvable_.nonneglect_.giT_.work_.top_._incomplete_.container_.undone_.wrought_.primed._bak_._upload.SettingsX.Default.push4nonchange;
 		}
 
 		private void checkBox1_CheckedChanged(object sender, EventArgs e)
 		{
-			nilnul.dev.bak.nilnul0.dev.src.sln.div_.unjoint_.nonneglect_.giT_.work_.top_._incomplete_.container_.undone_.wrought_.primed._bak_._upload.Settings1.Default.push4nonchange=this.checkBox1.Checked
+			nilnul.dev.srcs.each.sln.div_.delvable_.nonneglect_.giT_.work_.top_._incomplete_.container_.undone_.wrought_.primed._bak_._upload.SettingsX.Default.push4nonchange=this.checkBox1.Checked
 =
 			this.checkBox1.Checked  				;
 
-			nilnul.dev.bak.nilnul0.dev.src.sln.div_.unjoint_.nonneglect_.giT_.work_.top_._incomplete_.container_.undone_.wrought_.primed._bak_._upload.Settings1.Default.Save();
+			nilnul.dev.srcs.each.sln.div_.delvable_.nonneglect_.giT_.work_.top_._incomplete_.container_.undone_.wrought_.primed._bak_._upload.SettingsX.Default.Save();
 
 		}
 	}
